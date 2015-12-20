a239e28dfb63ab0bb1b8489dcd5845f4645c0007
Natsulus <Natsulus@hotmail.com>
2015-12-21 01:55:45 +1100

    Update Helper.js

--- Helper.js
+++ Helper.js
@@ -1,9 +1,11 @@
+const tabs = ["buildings", "jobs", "upgrades", "equipment", "helper"];
+
 var helperTab = document.createElement("LI");
 helperTab.setAttribute("role", "presentation");
 helperTab.setAttribute("id", "helperTab");
 helperTab.setAttribute("onclick", "filterTabs('helper')");
 helperTab.setAttribute("class", "buyTab");
-helperTab.setAttribute("style", "background: rgba(0, 0, 0, 0);");
+helperTab.setAttribute("style", "background: rgba(255, 255, 255, 0.25);");
 
 var helperA = document.createElement("A");
 helperA.setAttribute("id", "helperA");
@@ -11,9 +13,39 @@ helperA.setAttribute("href", "#");
 helperA.setAttribute("title", "");
 helperA.setAttribute("style", "border-bottom-width: 1px; border-bottom-style: solid; border-bottom-color: rgb(221, 221, 221);");
 
-var text = document.createTextNode("Helper");
+var helperTabText = document.createTextNode("Helper");
 
-helperA.appendChild(text);
+helperA.appendChild(helperTabText);
 helperTab.appendChild(helperA);
 
-document.getElementById("buyTabsUl").appendChild("helperTab");
+document.getElementById("buyTabsUl").appendChild(helperTab);
+
+
+
+// Create a Container using the example from container.html
+var helperContainer = document.createElement("DIV");
+helperContainer.setAttribute("style", "display: none;");
+
+var helperTitleDiv = document.createElement("DIV");
+helperTitleDiv.setAttribute("id", "helperTitleDiv");
+helperTitleDiv.setAttribute("class", "titleDiv");
+
+var helperTitleRow = document.createElement("DIV");
+helperTitleRow.setAttribute("class", "row");
+
+var helperTitleCol = document.createElement("DIV");
+helperTitleCol.setAttribute("class", "col-xs-4");
+
+var helperTitleSpan = document.createElement("SPAN");
+helperTitleSpan.setAttribute("id", "helperTitleSpan");
+helperTitleSpan.setAttribute("class", "titleSpan");
+
+var helperTitleText = document.createTextNode("Helper");
+
+helperTitleSpan.appendChild(helperTitleText);
+helperTitleCol.appendChild(helperTitleSpan);
+helperTitleRow.appendChild(helperTitleCol);
+helperTitleDiv.appendChild(helperTitleRow);
+helperContainer.appendChild(helperTitleDiv);
+
+document.getElementById("buyHere").appendChild(helperContainer);
